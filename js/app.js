39750b62a8fffd82b827c2a3fd300a52d29fa4bd
Chris Campbell <christopherecampbell@gmail.com>
2022-12-15 12:28:47 -0700

    initial stub up and css reset

---
+++ js/app.js
@@ -0,0 +1,37 @@
+/*---------------------------- Variables (state) ----------------------------*/
+
+
+
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+
+
+
+/*------------------------ Minimum Requirements -----------------------------*/
+// - **Render a game in the browser**. You may not use Canvas or jQuery.
+// - **Include win/loss logic and render win/loss messages in HTML.** You may not use alerts or prompt anywhere in your application.
+// - Include separate HTML, CSS, JavaScript, and JavaScript Data files.
+// - Be grammatically correct and be free of spelling errors.
+// - Display a favicon.
+// - Use CSS Flexbox or Grid.
+// - Look and feel similar to apps we use daily - it has should have a consistent and polished user interface and offers a positive user experience, including accessibility features.
+// - Have no remaining dead or commented out code, or console logs.
+// - Be coded using proper indentation.
+// - Be coded using function and variable names that make sense and follow the conventions demonstrated in lecture (for example: arrays are always plural).
+// - **Display that you are making consistent code choices**. For example, choose between your preference for function declarations vs. function expressions.
+// - Be deployed online so that the rest of the world can play your game!
+// - Include functionality specific to your game as specified in square brackets in the Recommended Games section below, or as discussed with your instructor if building a game not on the Recommended Games list.
