e5667df65f113a3ebcc077f389c3c86a4c4e3cf3
Chris Campbell <christopherecampbell@gmail.com>
2022-12-19 16:58:09 -0700

    add score reset to init

--- js/app.js
+++ js/app.js
@@ -128,6 +128,8 @@ scoreboard2El.addEventListener("click", scoreboardClickHandle)
 init()
 
 function init() {
+  p1Score = 0
+  p2Score = 0
   turnOver = false
   roundCounter = 1
   rollCounter = 0
